e2a217d5fdab0c7f171a24703416ab46850b8342
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-05 12:46:47 +0200

    Updated Core, added connection with database

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello")
+import pyodbc
+from datetime import datetime
+
+
+class Calculator:
+    def __init__(self):
+        self.history = []
+        self.connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=CalculatorApp;Trusted_Connection=yes'
+        self.conn = pyodbc.connect(self.connection_string)
+        self.cursor = self.conn.cursor()
+
+    def calculate(self, expression: str):
+        try:
+            result = eval(expression)
+            self.store_history(expression, result)
+            self.store_user_log(expression, result)
+            return result
+
+        except Exception as e:
+            return f"Ошибка: {e}"
+    
