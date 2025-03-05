9779f4b12cb847e6329b05b3385384fed31b9670
AlexBeT13 <saxarixa51@gmail.com>
2025-03-05 10:46:40 +0200

    update cal_core

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello")
+def core_calculator():
+    while True:
+        expr = input("Введите выражение ('exit' для выхода): ")
+        if expr.lower() == "exit":
+            print("Программа завершена.")
+            break
+        try:
+            result = eval(expr)
+            print(f"Результат: {result}")
+        except Exception as e:
+            print(f"Ошибка: {e}")
+
+if __name__ == "__main__":
+    core_calculator()
